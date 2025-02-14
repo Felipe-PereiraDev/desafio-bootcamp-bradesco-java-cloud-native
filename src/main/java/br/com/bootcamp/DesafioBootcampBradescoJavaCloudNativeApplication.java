0afb19b3927c84f1caf9988b249f0520c336b049
Felipe <felpera41@gmail.com>
2025-02-13 21:53:55 -0400

    Initial commit

---
+++ src/main/java/br/com/bootcamp/DesafioBootcampBradescoJavaCloudNativeApplication.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBootcampBradescoJavaCloudNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBootcampBradescoJavaCloudNativeApplication.class, args);
+	}
+
+}
